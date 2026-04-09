553a6f40e0f13fbfe746e60374775f6e1e55d0b7
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-09 14:16:22 +0530

    Add 02_datatypes file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -14,6 +14,12 @@ accountCity = "Damoh"
 console.log(accountId);
 
 
+/*
+  prefer not to use var
+  because of issue in block scope and functional scope
+*/
+
+
 console.log([accountEmail, accountPassword, accountCity, accountId]);
 console.table([accountEmail, accountPassword, accountCity, accountId]);
 
