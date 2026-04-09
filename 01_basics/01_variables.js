2ad6314caf41b50535562c3d26177243a9345370
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-09 14:02:19 +0530

    Add code to variables file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,7 +7,15 @@ accountCity = "Banglore"
 
 //accountId = 2 //not allowed because accountId is a constant
 
+accountEmail = "SahabSinghThakur@gmail.com"
+accountPassword = "Sahab"
+accountCity = "Damoh"
+
 console.log(accountId);
+
+
+console.log([accountEmail, accountPassword, accountCity, accountId]);
+console.table([accountEmail, accountPassword, accountCity, accountId]);
 
 
 
