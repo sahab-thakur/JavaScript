0d3e2cc0b02b9ea747b1fa33b11c07210c4a176c
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-10 14:39:32 +0530

    Add code to variables file

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all the code as newer version of js
+
+// alert("3 + 3 "); // We are using nodejs not browser so alert will not work!
+
+//code readability msutbe high and code should be maintainable
+
+let name = "Sahab"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint => more than 2 to power 53
+// string => " "
+//boolean => true or false
+// null => standalone value that represents nothing
+// undefined => value is not assigned to a variable
+// symbol => unique identifier
+
