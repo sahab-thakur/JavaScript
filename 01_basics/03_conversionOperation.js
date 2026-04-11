527107576c92e0033bfe55b4f9fd69eed8759670
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-11 16:35:37 +0530

    Add notes and understand conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,20 @@
-let score = "Sahab"
+let score = 33
 
 // console.log(typeof score);
 // console.log(typeof (score));
 
 let valueInNumber = Number(score) 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//notes
+//"33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0   
+//null => 0
+undefined => NaN
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
