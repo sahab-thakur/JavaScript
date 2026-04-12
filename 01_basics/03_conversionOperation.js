2e1fbd0f2d76db4fe71a687147c3d3c7cf929687
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-12 15:04:02 +0530

    comment code

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -60,14 +60,13 @@ let str1 = "Hello"
 let str2 = " Sahab"
 
 let str3 =  str1 + str2;
+// console.log(str3);
 
-console.log(str3);
-
-console.log("2" + 2);
-console.log(2 + "2");
-console.log("2" + 2 + 2);
-console.log(2+ 2 + "2");
-console.log("2" + (2+2));
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("2" + 2 + 2);
+// console.log(2+ 2 + "2");
+// console.log("2" + (2+2));
 
 
 
