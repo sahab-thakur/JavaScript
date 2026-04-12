cf1ebb9e2672b6aec1e8748e512881319187a33a
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-12 15:41:06 +0530

    comment code

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -69,5 +69,18 @@ let str3 =  str1 + str2;
 // console.log("2" + (2+2));
 
 
+console.log(true);
+console.log(+true)
+
+console.log("");
+console.log(+"");
 
 
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter =  100;
+gameCounter++; 
+console.log(gameCounter);
+gameCounter--;
+console.log(gameCounter);   
