698ede7a0b16ecd827b150874f7ae3199d398293
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-12 15:03:28 +0530

    Learn about Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -54,7 +54,7 @@ let negValue = -value;
 
 // console.log("2" + "2");
 
-console.log("2" + 2);
+
 
 let str1 = "Hello"
 let str2 = " Sahab"
@@ -62,6 +62,13 @@ let str2 = " Sahab"
 let str3 =  str1 + str2;
 
 console.log(str3);
+
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("2" + 2 + 2);
+console.log(2+ 2 + "2");
+console.log("2" + (2+2));
+
 
 
 
