d4d2f26b230b86d3c3f81141e596f54eca52721d
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-15 17:47:37 +0530

    Update COde

--- 01_basics/dataTypes_Summary.js
+++ 01_basics/dataTypes_Summary.js
@@ -10,9 +10,27 @@
 // 7. BigInt => 1234567890123456789012345678901234567890n
 
 
+const score = 100;  //number
+const scoreValue = 100.3; //number
+
+const isLoggedIn = false;  //boolean
+const outsideTemp = null;  //null   
+const userEmail = undefined;  //undefined
+
+const id = Symbol("123"); //symbol
+const anotherId = Symbol("123"); //symbol
+
+console.log(id === anotherId); //false
+console.log(id !=== anotherId); //true
+
+const bigNumber = 1234567890123456789012345678901234567890n; //bigint
+
+
+
+
 //Refrerence (Non primitive)
 
-// Array, objects, Functions
+// Array, Objects, Functions
 // 1. Array => [1, 2, 3, 4, 5], ["Hello", "World"], [1, "Hello", true]
 // 2. Object => { name: "John", age: 30, city: "New York" }
 // 3. Function => function add(a, b) { return a + b; }
