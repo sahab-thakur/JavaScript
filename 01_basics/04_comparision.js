b2ac4851f380f9566ff824b7798c19384595a40c
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-15 15:57:15 +0530

    Summary of dataTypes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,5 +5,16 @@
 // console.log(2==1);
 // console.log(2!=1);
 
-console.log("2">0);
-console.log("02">0);
+// console.log("2">0);
+// console.log("02">0);
+
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+console.log("=================");
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
---
+++ 01_basics/dataTypes_Summary.js
@@ -0,0 +1,18 @@
+//primitive
+
+// 7 Types of primitive data types in JavaScript
+// 1. String => "Hello", 'Hello', `Hello`
+// 2. Number => 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10
+// 3. Boolean => true or false
+// 4. Null => null
+// 5. Undefined => undefined
+// 6. Symbol => Symbol("Hello")
+// 7. BigInt => 1234567890123456789012345678901234567890n
+
+
+//Non primitive
+// Array, objects, Functions
+// 1. Array => [1, 2, 3, 4, 5], ["Hello", "World"], [1, "Hello", true]
+// 2. Object => { name: "John", age: 30, city: "New York" }
+// 3. Function => function add(a, b) { return a + b; }
+
