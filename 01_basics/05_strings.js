b4f7dac1a3326f116789dbb9cff625cab08dd0b7
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-16 22:01:28 +0530

    learn string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name =  "Sahab"; //String literal
+const repoCount = 50
+
+// console.log(name + repoCount + "value"); //old syntax now we use template literals
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);    //template literals morden syntax
+//  string interpolation, it is more readable and easier to use than string concatenation.
+
+const ganemName = new String("GTA"); //String constructor
+console.log(ganemName);
+
+
+
+
+
+
