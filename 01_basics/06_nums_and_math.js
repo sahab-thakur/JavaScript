f111d44ccfe4009f7a00848f7966e6b472ab5832
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-18 22:06:52 +0530

    learn toPrecision method in nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -8,3 +8,8 @@ console.log(balance.toString()); // toString() is a method of number objects, it
 
 console.log(balance.toString().length); // toString() is a method of number objects, it converts the number to a string, in this case it will give us "100" because the primitive number value of the balance object is 100. we can also use the String() function to convert a number to a string, for example String(balance) will also give us "100". length is a property of string objects, it gives us the length of the string, in this case it will give us 3 because "100" has 3 characters.
 console.log(balance.toFixed(2)); // toFixed() is a method of number objects, it formats the number to a specific number of decimal places, in this case it will give us "100.00" because we are formatting the number to 2 decimal places. if we take toFixed(0) it will give us "100" because it will round the number to the nearest integer. if we take toFixed(1) it will give us "100.0" because it will format the number to 1 decimal place.
+
+
+const numberOne = 23.89666
+
+console.log(numberOne.toPrecision(3)); // toPrecision() is a method of number objects, it formats the number to a specific number of significant digits, in this case it will give us "23.9" because we are formatting the number to 3 significant digits. if we take toPrecision(2) it will give us "24" because it will round the number to 2 significant digits. if we take toPrecision(4) it will give us "23.90" because it will format the number to 4 significant digits.
