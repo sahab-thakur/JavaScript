154ecb428ab5ce5b9ac968f808ea6ed14fc9575c
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-21 23:58:56 +0530

    learn about dateinJS and its conversions

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -12,6 +12,8 @@ console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+
 
 
 
