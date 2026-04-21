dea33210b30a7c69ba0be43b2ae93872a48d27b8
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-22 00:40:13 +0530

    learn about dateinJS and its conversions and add notes

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -50,6 +50,21 @@ console.log(myCreatedDate.getTime()); // this will give us the timestamp of the 
 console.log(Math.floor(Date.now()/1000));
 
 
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+//`${newDate.getDay()} and the time  `
+
+newDate.toLocaleString('default', {
+    weekday: 'long',
+    timeZone: 'Asia/KOlkata',
+    
+})
+
+
+
 
 
 
