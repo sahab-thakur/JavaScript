29e1222eda7b802ee8b85eaa7a4d530aa09254a8
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-22 21:43:32 +0530

    learn about array methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,3 +2,26 @@
 
 const arr = [0,1,2,3,4,5]
 const myArr = ["she", "he", "it", "we"]
+
+const myArr2= new Array("she", "he", "it", "we")
+console.log(arr);
+
+
+//Array methods
+
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+
+// arr.unshift(-1)
+// arr.shift()
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+
+const newArr = arr.join()
+
+console.log(arr);
+
+console.log(newArr);
+
