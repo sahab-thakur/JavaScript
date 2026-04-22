53621ba09e0f6579e66b57c839ddd4832b7a08c3
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-22 21:54:27 +0530

    learn about slice and splice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,7 +4,7 @@ const arr = [0,1,2,3,4,5]
 const myArr = ["she", "he", "it", "we"]
 
 const myArr2= new Array("she", "he", "it", "we")
-console.log(arr);
+// console.log(arr);
 
 
 //Array methods
@@ -30,12 +30,22 @@ console.log(arr);
 
 //slice , splice
 
-console.log("a", arr);
 
-const myn1 =arr.slice(2,4);
+//Slice
+console.log("A", arr);
+
+const myn1 =arr.slice(1,3);
 
 console.log(myn1);
-console.log("b", arr);
+
+// Splice
+console.log("B", arr);
+const myn2 = arr.splice(1, 3);
+
+console.log("C", arr);
+
+console.log(myn2);
+
 
 
 
