2c9972e878e7fa505de5ce80c9ecc0cd7c0cb9b9
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-24 22:15:06 +0530

    learn about arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,2 +1,6 @@
 const marvelHeroes = ["Ironman", "Captain America", "Thor", "Hulk", "Black Widow"]; 
 const dcHeroes = ["Superman", "Batman", "Flash"];
+
+marvelHeroes.push(dcHeroes); //push adds the entire array as a single element to the end of the array
+
+console.log(marvelHeroes); //["Ironman", "Captain America", "Thor", "Hulk", "Black Widow", ["Superman", "Batman", "Flash"]]
