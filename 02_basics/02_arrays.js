0cee6415c507dcfdf9f6a2989c31026056491c00
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-26 22:57:44 +0530

    learn about arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,6 +1,14 @@
 const marvelHeroes = ["Ironman", "Captain America", "Thor", "Hulk", "Black Widow"]; 
 const dcHeroes = ["Superman", "Batman", "Flash"];
 
-marvelHeroes.push(dcHeroes); //push adds the entire array as a single element to the end of the array
+// marvelHeroes.push(dcHeroes); //push adds the entire array as a single element to the end of the array
 
-console.log(marvelHeroes); //["Ironman", "Captain America", "Thor", "Hulk", "Black Widow", ["Superman", "Batman", "Flash"]]
+// console.log(marvelHeroes); //["Ironman", "Captain America", "Thor", "Hulk", "Black Widow", ["Superman", "Batman", "Flash"]]
+
+// console.log(marvelHeroes[3][1]); //undefined because marvelHeroes[3] is "Hulk" which is a string and strings do not have an index 1
+
+const allHeros=marvelHeroes.concat(dcHeroes); //concat does not modify the original array but returns a new array
+
+ console.log(allHeros); //["Ironman", "Captain America", "Thor", "Hulk", "Black Widow", ["Superman", "Batman", "Flash"]]
+
+ 
