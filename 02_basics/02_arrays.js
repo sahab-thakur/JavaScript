1c4c5837641a8e1efd7b3b5b6f29282760001a26
Sahab Singh Thakur <thakursahabsingh12@gmail.com>
2026-04-28 13:47:44 +0530

    no to array convesion

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -28,8 +28,11 @@ const real_another_array = another_array.flat(Infinity);
 
  console.log((Array.from({name:"Sahab"}))); //this will give empty array because we have to tell to make array of keys or values // interesting fo interviews
  
- 
-  
+
+ let score1 = 100
+ let score2 = 200
+ let score3 = 300
+
+ console.log(Array.of(score1, score2, score3));
  
 
- 
